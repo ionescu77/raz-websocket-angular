bb44db38ba34d89e15e8f6fa17fdb196752007a3
ionescu77 <ionescu77@gmail.com>
2021-04-15 13:31:13 +0300

    original article but does not work #2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'chat', component: ChatComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
